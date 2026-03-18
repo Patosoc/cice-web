94af1dfa166b23b8f5328b28958c46f6a12984db
Patosoc <patosoc@gmail.com>
2026-03-18 17:30:06 -0500

    cambios detalles

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -11,22 +11,21 @@ export default function Hero() {
       <div className="max-w-5xl mx-auto text-center px-6">
 
         <Image
-          src="/logo-cice.svg"
-          alt="Centro de Innovación y Ciberseguridad Educativa"
+          src="/logo-cice.jpeg"
+          alt="Centro de Investigación en Ciberseguridad Educativa"
           width={150}
           height={150}
           className="mx-auto mb-6"
         />
 
         <h1 className="text-4xl font-bold text-blue-900 mb-6">
-          Centro de Innovación y Ciberseguridad Educativa
+          Centro de Investigación en Ciberseguridad Educativa
         </h1>
 
         <p className="text-gray-700 text-lg">
-          Iniciativa dedicada a la investigación,
-          formación e innovación en ciberseguridad
-          orientada a la protección de estudiantes
-          en entornos educativos digitales.
+          Iniciativa estratégia orientada a transformar la manera en que los sistemas educativos
+          abordan la protección del bienestar infantil y juvenil en contextos mediados por la 
+          tecnología.
         </p>
 
       </div>
